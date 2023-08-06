b4eac41cd14074193761214f76b768b4fa15fa17
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2023-08-07 02:18:10 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Ergin TIRAVOĞLU</h1>
+        <h2>Under Construction</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
