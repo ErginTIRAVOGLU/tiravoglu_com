b67e25022fdf0b2e0cd6f95bb7c5b648f03af40f
Ergin <ergintiravoglu@msn.com>
2026-09-20 21:01:38 +0300

    fix logo error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+ 
 import './App.css';
 
 function App() {
@@ -9,15 +9,15 @@ function App() {
         <h2>Under Construction</h2>
       </header>
     <ul>
-    <li><a href="https://tiravoglu.com/">Tiravoglu.com</a></li>
-    <li><a href="https://medocanlireklam.vercel.app/">Canlı Yayın Reklam</a></li>
-    <li><a href="https://medo-sorusor.vercel.app/">Soru Yarışması</a></li>
-    <li><a href="https://medoqrmenu.vercel.app/">QrMenu</a></li>
-    <li><a href="https://medovideokurs.vercel.app/">Online Toplantı Canlı Kurs</a></li>
-    <li><a href="https://lova-sadakat.vercel.app/">Sadakat Takip</a></li>
-    <li><a href="https://lovadigitalmenu.vercel.app/">Digital Signage</a></li>
-    <li><a href="https://lova-qrlist.vercel.app/">QrList</a></li>
-    <li><a href="https://lovastream.vercel.app/">Stream Tips</a></li>
+      <li><a href="https://tiravoglu.com/">Tiravoglu.com</a></li>
+      <li><a href="https://medocanlireklam.vercel.app/">Canlı Yayın Reklam</a></li>
+      <li><a href="https://medo-sorusor.vercel.app/">Soru Yarışması</a></li>
+      <li><a href="https://medoqrmenu.vercel.app/">QrMenu</a></li>
+      <li><a href="https://medovideokurs.vercel.app/">Online Toplantı Canlı Kurs</a></li>
+      <li><a href="https://lova-sadakat.vercel.app/">Sadakat Takip</a></li>
+      <li><a href="https://lovadigitalmenu.vercel.app/">Digital Signage</a></li>
+      <li><a href="https://lova-qrlist.vercel.app/">QrList</a></li>
+      <li><a href="https://lovastream.vercel.app/">Stream Tips</a></li>
     </ul>
     </div>
   );
