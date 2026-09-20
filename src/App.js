fad74c69e24dd6db95eaf8f71e1678f58d62e077
Ergin <ergintiravoglu@msn.com>
2026-09-20 20:37:47 +0300

    Add links to various projects in App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,17 @@ function App() {
         <h1>Ergin TIRAVOĞLU</h1>
         <h2>Under Construction</h2>
       </header>
+    <ul>
+    <li><a href="https://tiravoglu.com/">Tiravoglu.com</a></li>
+    <li><a href="https://medocanlireklam.vercel.app/">Canlı Yayın Reklam</a></li>
+    <li><a href="https://medo-sorusor.vercel.app/">Soru Yarışması</a></li>
+    <li><a href="https://medoqrmenu.vercel.app/">QrMenu</a></li>
+    <li><a href="https://medovideokurs.vercel.app/">Online Toplantı Canlı Kurs</a></li>
+    <li><a href="https://lova-sadakat.vercel.app/">Sadakat Takip</a></li>
+    <li><a href="https://lovadigitalmenu.vercel.app/">Digital Signage</a></li>
+    <li><a href="https://lova-qrlist.vercel.app/">QrList</a></li>
+    <li><a href="https://lovastream.vercel.app/">Stream Tips</a></li>
+    </ul>
     </div>
   );
 }
