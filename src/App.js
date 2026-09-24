7ff846bd27d420581b1562b718e165a2840e5a66
Ergin <ergintiravoglu@msn.com>
2026-09-24 19:04:13 +0300

    Fix link for Sadakat Takip in App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           <li><a href="https://medo-sorusor.vercel.app/">Soru Yarışması</a></li>
           <li><a href="https://medoqrmenu.vercel.app/">QrMenu</a></li>
           <li><a href="https://medovideokurs.vercel.app/">Online Toplantı Canlı Kurs</a></li>
-          <li><a href="https://lova-sadakat.vercel.app/">Sadakat Takip</a></li>
+          <li><a href="https://lovasadakat.vercel.app/">Sadakat Takip</a></li>
           <li><a href="https://lovadigitalmenu.vercel.app/">Digital Signage</a></li>
           <li><a href="https://lova-qrlist.vercel.app/">QrList</a></li>
           <li><a href="https://lovastream.vercel.app/">Stream Tips</a></li>
